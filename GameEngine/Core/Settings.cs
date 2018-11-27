5bcfb60d961d2f633c649afd7cc3974d648f9b27
AlvinGames <kanatush.tim@gmail.com>
2018-11-27 23:15:44 +0300

    Game settings

---
+++ GameEngine/Core/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Core
+{
+    public class Settings
+    {
+        public int Music { get; set; }
+        public int Value { get; set; }
+
+        public bool FullScreen { get; set; }
+
+        public Settings()
+        {
+            
+        }
+    }
+}
